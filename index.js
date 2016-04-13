db18744528a676db1bc332f4cb3d890657a6131c
Eric Chu <eric@ericchu.net>
2016-04-13 00:09:08 -0400

    Convert to koa style

--- index.js
+++ index.js
@@ -16,44 +16,55 @@ const DEFAULT_PAYLOAD_PATH = path.join(FILES, DEFAULT_PAYLOAD);
 const PATH_CHANGER = './pathchanger';
 let counter = 0;
 
-app.use(ctx => {
+app.use((ctx, next) => {
   const payload = ctx.request.url.slice(1, MAX_CHARS + 1);
+  ctx.payload = payload;
 
   if (payload.length == 0) {
     ctx.body = `<input maxlength="${MAX_CHARS}" placeholder="${DEFAULT_PAYLOAD}" type="text" id="path" /><input type="button" value="Download" onclick="window.location.href = document.getElementById('path').value" />`;
     return;
   }
 
-  console.log(`[${payload}] Serving`);
-  const file = path.join(FILES, payload);
-  fs.exists(file).then(exists => {
-    if (!exists) {
-      const id = counter;
-      counter++;
-      const tmp = path.join(TMP, id.toString());
+  return next();
+});
 
-      console.log(`[${payload}] Copying ${DEFAULT_PAYLOAD} to ${tmp}`);
-      return fs.copy(DEFAULT_PAYLOAD_PATH, tmp).then(() => {
-        console.log(`[${payload}] Running ${PATH_CHANGER} on ${tmp}`);
-        const cpPromise = child_process.execFile(PATH_CHANGER, [tmp]);
-        const stdin = cpPromise.childProcess.stdin;
-        stdin.write(payload);
-        stdin.end();
-        return cpPromise;
-      }).then(() => {
-        console.log(`[${payload}] Renaming ${tmp} to ${file}`);
-        return fs.rename(tmp, file);
-      });;
-    } else {
-      console.log(`[${payload}] Found`);
-    }
-  }).then(() => {
-    console.log(`[${payload}] Sending`);
-    console.log(ctx, file);
-    send(ctx, file);
-  }).catch(err => {
-    console.log(err);
+app.use((ctx, next) => {
+  console.log(`[${ctx.payload}] Serving`);
+  const file = path.join(FILES, ctx.payload);
+  ctx.file = file;
+  return fs.exists(file).then(exists => {
+    ctx.exists = exists;
+    return next();
   });
+});
+
+app.use((ctx, next) => {
+  if (!ctx.exists) {
+    const id = counter;
+    counter++;
+    const tmp = path.join(TMP, id.toString());
+
+    console.log(`[${ctx.payload}] Copying ${DEFAULT_PAYLOAD} to ${tmp}`);
+    return fs.copy(DEFAULT_PAYLOAD_PATH, tmp).then(() => {
+      console.log(`[${ctx.payload}] Running ${PATH_CHANGER} on ${tmp}`);
+      const cpPromise = child_process.execFile(PATH_CHANGER, [tmp]);
+      const stdin = cpPromise.childProcess.stdin;
+      stdin.write(ctx.payload);
+      stdin.end();
+      return cpPromise;
+    }).then(() => {
+      console.log(`[${ctx.payload}] Renaming ${tmp} to ${ctx.file}`);
+      return fs.rename(tmp, ctx.file).then(next);
+    });
+  } else {
+    console.log(`[${ctx.payload}] Found`);
+    return next();
+  }
+});
+
+app.use(ctx => {
+  console.log(`[${ctx.payload}] Sending`);
+  return send(ctx, ctx.file);
 });
 
 app.listen(3000);
