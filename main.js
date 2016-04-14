7d36f329ebc90e5d7806419495890b32f7641332
Eric Chu <eric@ericchu.net>
2016-04-13 22:53:50 -0400

    Add templates

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ const Koa = require('koa');
 const send = require('koa-send');
 const cors = require('koa-cors');
 const convert = require('koa-convert');
+const views = require('koa-views');
+const router = require('koa-router')();
 const app = new Koa();
 
 const MAX_CHARS = 37;
@@ -42,56 +44,56 @@ app.use(convert(cors({
   origin: '*',
 })));
 
-app.use((ctx, next) => {
-  const payload = ctx.request.url.slice(1, MAX_CHARS + 1);
-  ctx.payload = payload;
+app.use(views(__dirname + '/views', {
+  map: {
+    html: 'ejs',
+  },
+}));
 
-
-  if (payload.length == 0) {
-    logger.info('Serving /');
-    const url = `http://${ctx.host}/`;
-    ctx.body = `<p>sdmc:/ <input onkeyup="if (event.keyCode == 13) window.location.href = '${url}' + event.target.value" maxlength="${MAX_CHARS}" placeholder="${PAYLOAD}" type="text" /> <input type="button" value="Press enter you shit" disabled /></p><p>Download the latest AuReiNand with customized payload path!</p><p><strong>Examples:</strong></p><ul><li>arm9payload.bin</li><li>a9lh/AuReiNand.bin</li><li>arm9select/default.bin</li></ul><p><strong>Notes:</strong></p><ul><li>Do not put a leading slash</li><li>Use forward slashes</li><li>Path can be 37 characters max</li></ul><p>Check out #3dshacks on Rizon for more information.</p><p>Binary from <a href="http://astronautlevel2.github.io/AuReiNand/">http://astronautlevel2.github.io/AuReiNand/</a></p><p>Source code here: <a href="https://github.com/ericchu94/arn">GitHub</a></p>`;
-    return;
-  }
-
-  return next();
+router.get('/', (ctx, next) => {
+  return ctx.render('index', {
+    placeholder: PAYLOAD,
+    maxLength: MAX_CHARS,
+    url: `http://${ctx.host}/`,
+  });
 });
 
-app.use((ctx, next) => {
+router.get(['/latest/*', '/release/*'], (ctx, next) => {
+  if (ctx.req.url.startsWith('/latest/'))
+    ctx.version = LATEST;
+  else
+    ctx.version = RELEASE;
+  ctx.payload = ctx.params[0];
+  return next();
+}, (ctx, next) => {
   const id = counter;
   counter++;
   const tmpDir = id.toString();
   ctx.tmpDir = tmpDir;
   logger.info(`Creating directory ${tmpDir}`, { payload: ctx.payload });
   return fs.mkdirs(tmpDir).then(next);
-});
-
-app.use((ctx, next) => {
+}, (ctx, next) => {
   const tmp = path.join(ctx.tmpDir, PAYLOAD);
-  ctx.version = LATEST;
   ctx.tmp = tmp;
   logger.info(`Copying ${ctx.version} to ${tmp}`, { payload: ctx.payload });
   return fs.copy(ctx.version, tmp).then(next);
-});
-
-app.use((ctx, next) => {
+}, (ctx, next) => {
   logger.info(`Running ${PATH_CHANGER} on ${ctx.tmp}`, { payload: ctx.payload });
   const cpPromise = child_process.execFile(PATH_CHANGER, [ctx.tmp]);
   const stdin = cpPromise.childProcess.stdin;
   stdin.write(ctx.payload);
   stdin.end();
   return cpPromise.then(next);
-});
-
-app.use((ctx, next) => {
+}, (ctx, next) => {
   logger.info(`Sending ${ctx.tmp}`, { payload: ctx.payload });
   return send(ctx, ctx.tmp).then(next);
-});
-
-app.use((ctx) => {
+}, (ctx) => {
   logger.info(`Deleting ${ctx.tmpDir}`, { payload: ctx.payload });
   return fs.remove(ctx.tmpDir);
 });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 function update() {
   logger.info('Updating binary');
